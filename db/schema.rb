cd1dd385e2acc33d8dbe82f1abb5592efa56906c
Michael Nutt <michael@nuttnet.net>
2008-11-08 22:58:29 -0500

    dummy commit

--- db/schema.rb
+++ db/schema.rb
@@ -62,8 +62,8 @@ ActiveRecord::Schema.define(:version => 20081108222727) do
     t.datetime "updated_at"
   end
 
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
